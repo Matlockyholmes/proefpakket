16217d1c054e4de675c9f25e4bae8f4618ee77a3
matthias.heylen <matthiasheylen1@gmail.com>
2019-07-17 11:19:57 +0200

    "Met Ondernemingsnummer toevoegen"

--- src/main/java/be/vdab/proefpakket/ProefpakketApplication.java
+++ src/main/java/be/vdab/proefpakket/ProefpakketApplication.java
@@ -2,6 +2,9 @@ package be.vdab.proefpakket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 public class ProefpakketApplication {
